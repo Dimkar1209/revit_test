7281ca20cb26984978f50ff9cc8aba6f7cae66e8
LAPTOP-E41PE1SP\Dimkar <dimkarmk3@gmail.com>
2024-01-24 21:49:19 +0200

    Task 10001: Hello word basic cmd UI added

---
+++ Revit_Conduit_Table/Revit_Conduit_Table/HelloWordApp.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.UI;
+using Revit_Conduit_Table;
+using Revit_Conduit_Table.Resources;
+using System.Reflection;
+
+namespace RevitHelloWorld
+{
+    public class HelloWordApp : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            application.CreateRibbonTab(UI_Text.ADDIN_TAB_NAME);
+
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(UI_Text.ADDIN_TAB_NAME, UI_Text.ADDIN_PANEL_NAME);
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            var buttonData = new PushButtonData(
+                "HelloWordCmd",
+                "Hello World",
+                thisAssemblyPath,
+                HelloWordCmd.CommandPath);
+
+            var pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Revit_Conduit_Table/Revit_Conduit_Table/HelloWordCmd.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+
+namespace Revit_Conduit_Table
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class HelloWordCmd : IExternalCommand
+    {
+        public static string CommandPath => "Revit_Conduit_Table.HelloWordCmd";
+
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            try
+            {
+                TaskDialog.Show("Hello World", "Hello, Revit!");
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+
+    }
+}
